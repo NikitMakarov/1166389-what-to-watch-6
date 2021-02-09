09ecc26010368645397e9ffd01ed474fdf1168d9
Nikita Makarov <makarov@htmlacademy.ru>
2021-02-09 16:17:41 +0700

    Маршруты

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,45 @@
 import React from "react";
 import MainPage from "../main/render-main-page";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
+import FilmFullInfo from "../film/film-card";
+import MyList from "../list/my-list";
+import AddReview from "../review/add-review";
+import Player from "../player/player";
+import SignIn from "../signin/login";
+import PageError from "../error/error-404";
 
 const App = () => {
   return (
-    <MainPage
-      films={[`1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `10`, `11`, `12`, `13`, `14`, `15`, `16`, `17`, `18`, `19`, `20`]}
-      title={`The Grand Budapest Hotel`}
-      genre={`Comedy`}
-      date={2012}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainPage
+            films={[`1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `10`, `11`, `12`, `13`, `14`, `15`, `16`, `17`, `18`, `19`, `20`]}
+            title={`The Grand Budapest Hotel`}
+            genre={`Comedy`}
+            date={2012}
+          />
+        </Route>
+        <Route exact path="/films/:id">
+          <FilmFullInfo />
+        </Route>
+        <Route exact path="/mylist">
+          <MyList />
+        </Route>
+        <Route exact path="/films/:id/review">
+          <AddReview />
+        </Route>
+        <Route exact path="/player/:id">
+          <Player />
+        </Route>
+        <Route exact path="/login">
+          <SignIn />
+        </Route>
+        <Route>
+          <PageError />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
